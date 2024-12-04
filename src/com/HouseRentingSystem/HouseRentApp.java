82f4b763f50b5d15de31351b7e299563b64a9582
JoshuaZSong <joshuasong94@outlook.com>
2024-12-04 13:45:23 -0700

    Update HouseRentApp.java

--- src/com/HouseRentingSystem/HouseRentApp.java
+++ src/com/HouseRentingSystem/HouseRentApp.java
@@ -1,10 +1,13 @@
 package com.HouseRentingSystem;
 
+import com.HouseRentingSystem.view.HouseView;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HouseRentApp {
     public static void main(String[] args) {
         //Create HouseView class and show menu,entry of the software
-
+        new HouseView().mainMenu();
+        System.out.println("===You exit the system===");
     }
 }
