8d1840d2d97dfbb3fef36fa204e81062e51417a3
JoshuaZSong <joshuasong94@outlook.com>
2024-12-04 13:41:05 -0700

    Update HouseView.java

--- src/com/HouseRentingSystem/view/HouseView.java
+++ src/com/HouseRentingSystem/view/HouseView.java
@@ -1,5 +1,7 @@
 package com.HouseRentingSystem.view;
 
+import com.HouseRentingSystem.utils.Utility;
+
 /**
  * 1.Menu display
  * 2.Receive user's input
@@ -20,6 +22,17 @@ public class HouseView {
             System.out.println("\t\t\t4 Change House Info");
             System.out.println("\t\t\t5 House List");
             System.out.println("\t\t\t6 Exit");
+            System.out.println("Please select from 1-6:");
+            key = Utility.readChar();
+            switch(key){
+                case '1':;
+                case '2':;
+                case '3':;
+                case '4':;
+                case '5':;
+                case '6':;
+            }
+
         }while(loop);
     }
 }
