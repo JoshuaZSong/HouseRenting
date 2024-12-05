f04c8b7a64e12a15c078c873fc1bc44975aae273
JoshuaZSong <joshuasong94@outlook.com>
2024-12-05 13:58:13 -0700

    Update HouseService.java

--- src/com/HouseRentingSystem/service/HouseService.java
+++ src/com/HouseRentingSystem/service/HouseService.java
@@ -5,4 +5,11 @@ package com.HouseRentingSystem.service;
  * 2.CRUD
  */
 public class HouseService {
+
+    private House[] houses;// house object
+
+    public HouseService(int size){
+        //new houses
+        houses = new House[size]
+    }
 }
