a61f730aebfffb61187467eaff61b7899f195a8d
JoshuaZSong <joshuasong94@outlook.com>
2024-12-06 00:10:47 -0700

    Update HouseService.java

--- src/com/HouseRentingSystem/service/HouseService.java
+++ src/com/HouseRentingSystem/service/HouseService.java
@@ -1,5 +1,7 @@
 package com.HouseRentingSystem.service;
 
+import com.HouseRentingSystem.domin.House;
+
 /**
  * 1. Call from HouseView
  * 2.CRUD
@@ -10,6 +12,13 @@ public class HouseService {
 
     public HouseService(int size){
         //new houses
-        houses = new House[size]
+        houses = new House[size];//
+        //For testing,initial a test object
+        houses[0] = new House(1,"Josh","(111)111-1111","Calgary",1400,"Vacant");
+    }
+
+    //
+    public House[] list(){
+        return houses;
     }
 }
